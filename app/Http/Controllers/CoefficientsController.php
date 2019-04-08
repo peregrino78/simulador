c2101ea44cf483433dc5c80487fa0d48b6d7be5e
Igor Santos <igors.d@hotmail.com>
2019-04-08 16:35:54 -0300

    add funcoes para visualizar coeficientes

--- app/Http/Controllers/CoefficientsController.php
+++ app/Http/Controllers/CoefficientsController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coefficient;
 use Illuminate\Http\Request;
 
 class CoefficientsController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $coefficients = Coefficient::orderBy('data')->paginate();
+        return view('dashboard.coefficients.index', compact('coefficients'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $coefficient = Coefficient::findOrFail($id);
+
+        return view('dashboard.coefficients.show', compact('coefficient'));
+    }
 }
