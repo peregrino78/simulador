1c92c6048afbbc8eb73421e20b337f7b7287a296
Igor Santos <igors.d@hotmail.com>
2019-04-08 17:23:25 -0300

    criada funcao para cadastrar coeficiente

--- app/Http/Controllers/CoefficientsController.php
+++ app/Http/Controllers/CoefficientsController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Agreement;
 use App\Models\Coefficient;
 use Illuminate\Http\Request;
+use App\Models\OperationType;
 
 class CoefficientsController extends Controller
 {
@@ -14,7 +16,7 @@ class CoefficientsController extends Controller
      */
     public function index()
     {
-        $coefficients = Coefficient::orderBy('data')->paginate();
+        $coefficients = Coefficient::orderBy('date')->paginate();
         return view('dashboard.coefficients.index', compact('coefficients'));
     }
 
@@ -29,5 +31,55 @@ class CoefficientsController extends Controller
         $coefficient = Coefficient::findOrFail($id);
 
         return view('dashboard.coefficients.show', compact('coefficient'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $agreements = Agreement::pluck('name', 'id');
+        $operations = OperationType::pluck('name', 'id');
+        return view('dashboard.coefficients.create', compact('agreements', 'operations'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $rules = [
+            'term'              => 'required',
+            'rate'              => 'required',
+            'factor'            => 'required',
+            'date'              => 'required',
+            'agreement_id'      => 'required',
+            'operation_type_id' => 'required'
+        ];
+
+        $nicenames = [
+            'term'              => 'Prazo',
+            'rate'              => 'Taxa',
+            'factor'            => 'Fator',
+            'date'              => 'Data',
+            'agreement_id'      => 'Convênio',
+            'operation_type_id' => 'Operação'
+        ];
+
+        $this->validate($request, $rules, [], $nicenames);
+
+        $data = $request->request->all();
+
+        Coefficient::create($data);
+
+        flash('Coeficiente adicionado com sucesso.')->success()->important();
+
+        return redirect()->route('coeficientes.index');
     }
 }
