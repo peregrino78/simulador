ac0c935de9d3afa2703299a78409980fd393327a
Igor Santos <igors.d@hotmail.com>
2019-04-08 17:39:24 -0300

    incluida funcionalidade para edicao de coeficiente

--- app/Http/Controllers/CoefficientsController.php
+++ app/Http/Controllers/CoefficientsController.php
@@ -40,7 +40,6 @@ class CoefficientsController extends Controller
      */
     public function store(Request $request)
     {
-
         $rules = [
             'term'              => 'required',
             'rate'              => 'required',
@@ -66,6 +65,62 @@ class CoefficientsController extends Controller
         Coefficient::create($data);
 
         flash('Coeficiente adicionado com sucesso.')->success()->important();
+
+        return redirect()->route('coeficientes.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $coefficient = Coefficient::findOrFail($id);
+
+        $agreements = Agreement::pluck('name', 'id');
+        $operations = OperationType::pluck('name', 'id');
+
+        return view('dashboard.coefficients.edit', compact('coefficient', 'agreements', 'operations'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $coefficient = Coefficient::findOrFail($id);
+
+        $rules = [
+            'term'              => 'required',
+            'rate'              => 'required',
+            'factor'            => 'required',
+            'date'              => 'required',
+            'agreement_id'      => 'required',
+            'operation_type_id' => 'required'
+        ];
+
+        $nicenames = [
+            'term'              => 'Prazo',
+            'rate'              => 'Taxa',
+            'factor'            => 'Fator',
+            'date'              => 'Data',
+            'agreement_id'      => 'Convênio',
+            'operation_type_id' => 'Operação'
+        ];
+
+        $this->validate($request, $rules, [], $nicenames);
+
+        $data = $request->request->all();
+
+        $coefficient->update($data);
+
+        flash('Coeficiente atualizado com sucesso.')->success()->important();
 
         return redirect()->route('coeficientes.index');
     }
