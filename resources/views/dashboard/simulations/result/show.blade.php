dda19ce33ef76df77dc70ac1cd989183fdd4c09d
Igor Santos <igors.d@hotmail.com>
2019-04-09 17:00:50 -0300

    tela inicial para resultado de simulacao

---
+++ resources/views/dashboard/simulations/result/show.blade.php
@@ -0,0 +1,32 @@
+@extends('dashboard.templates.create')
+
+@section('title', 'Resultado da Simulação')
+
+@section('content')
+
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card-box">
+            <div class="tab-content">
+                <div class="tab-pane active show" id="info-b2">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title">Confira o Resultado</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <div class="row">
+                                                             
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="btn btn-success">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
